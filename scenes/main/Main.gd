efcb5412510dfeb9df50d89c7e2bac739b5abe41
TrueSunGaming <79117622+TrueSunGaming@users.noreply.github.com>
2024-11-28 13:12:07 -0500

    feat: load last environment from save

--- scenes/main/Main.gd
+++ scenes/main/Main.gd
@@ -2,3 +2,7 @@ class_name Main extends Node2D
 
 func _ready() -> void:
 	refs.main = self
+	
+	SaveData.load_from("user://save.tres").make_active()
+	
+	refs.env.env_id = refs.save.last_environment
