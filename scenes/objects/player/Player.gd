6438da66b114ca67efb1feeeee34c582efc212df
TrueSunGaming <79117622+TrueSunGaming@users.noreply.github.com>
2024-11-30 17:16:08 -0500

    feat: implement camera zoom

--- scenes/objects/player/Player.gd
+++ scenes/objects/player/Player.gd
@@ -1,6 +1,12 @@
 class_name Player extends CharacterBody2D
 
 @export var base_movement_speed: float
+@export var min_zoom: float
+@export var max_zoom: float
+@export var zoom_factor: float
+@export var zoom_time: float
+
+@onready var _goal_zoom: float = $Camera2D.zoom.x
 
 func _ready() -> void:
 	refs.player = self
@@ -12,3 +18,9 @@ func _physics_process(delta: float) -> void:
 
 func _process(delta: float) -> void:
 	rotation = global_position.angle_to_point(get_global_mouse_position())
+	
+	$Camera2D.zoom = $Camera2D.zoom.lerp(Vector2(_goal_zoom, _goal_zoom), delta / zoom_time)
+
+func _input(event: InputEvent) -> void:
+	_goal_zoom *= zoom_factor ** (int(Input.is_action_pressed("zoom_in")) - int(Input.is_action_pressed("zoom_out")))
+	_goal_zoom = clampf(_goal_zoom, min_zoom, max_zoom)
