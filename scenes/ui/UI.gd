9b558a17dec0dc338229d82d8df9c20698547687
TrueSunGaming <79117622+TrueSunGaming@users.noreply.github.com>
2024-12-04 20:49:28 -0500

    feat: add ui to refs

---
+++ scenes/ui/UI.gd
@@ -0,0 +1,4 @@
+class_name UI extends CanvasLayer
+
+func _ready() -> void:
+	refs.ui = self
--- scripts/refs.gd
+++ scripts/refs.gd
@@ -4,3 +4,4 @@ var main: Main
 var player: Player
 var env: EnvironmentManager
 var save: SaveData
+var ui: UI
