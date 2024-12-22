0fa20d17c659200834347dd7926113336b4cb1df
TrueSunGaming <79117622+TrueSunGaming@users.noreply.github.com>
2024-12-22 10:49:29 -0500

    feat: implement debug wipe inventory

--- scenes/ui/gui/debug/DebugWindow.gd
+++ scenes/ui/gui/debug/DebugWindow.gd
@@ -4,3 +4,6 @@ func _on_give_resource_pressed() -> void:
 	var id: String = $PanelContainer/VBoxContainer/GiveResource/ResourceID.text
 	var qty := int($PanelContainer/VBoxContainer/GiveResource/Quantity.text)
 	refs.player.inventory.add(ResourceStack.new(id, qty, true))
+
+func _on_wipe_inventory_pressed() -> void:
+	refs.player.inventory.content = []
