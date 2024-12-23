3e3724563cddc43c0e86cf49d79f6b2d6dc8c76e
TrueSunGaming <79117622+TrueSunGaming@users.noreply.github.com>
2024-12-22 21:14:39 -0500

    feat: implement moving items between storages

--- scenes/ui/resource/MenuResourceStackDisplay.gd
+++ scenes/ui/resource/MenuResourceStackDisplay.gd
@@ -1,7 +1,15 @@
 class_name MenuResourceStackDisplay extends PanelContainer
 
+signal grabbed(event)
+
 @export var stack: ResourceStack:
 	set(val):
 		if stack == val: return
 		stack = val
 		$ResourceStackDisplay.stack = val
+
+func _gui_input(event: InputEvent) -> void:
+	if not global.held_item and event is InputEventMouseButton and event.is_released():
+		grabbed.emit(event)
+		(event as InputEventMouseButton).canceled = true
+		print("grabbed")
--- scenes/ui/resource/storage/StorageDisplay.gd
+++ scenes/ui/resource/storage/StorageDisplay.gd
@@ -2,6 +2,8 @@ class_name StorageDisplay extends GridContainer
 
 const resource_display: PackedScene = preload("res://scenes/ui/resource/MenuResourceStackDisplay.tscn")
 
+signal node_removed(idx)
+
 @export var storage: StorageAccess:
 	set(val):
 		if storage == val: return
@@ -22,13 +24,18 @@ const resource_display: PackedScene = preload("res://scenes/ui/resource/MenuReso
 		
 		fill_empty()
 
-@export var fill_full_line := false
+@export var fill_full_line := false:
+	set(val):
+		if fill_full_line == val: return
+		fill_full_line = val
+		
+		fill_empty()
 
 var fill_empty_target: int:
 	get:
 		if not fill_full_line: return fill_empty_until
 		
-		return columns * ceili((maxf(storage.get_content().size(), fill_empty_until) + 1) / columns)
+		return columns * ceili(maxf((storage.get_content() if storage else []).size() + 1, fill_empty_until) / columns)
 
 func fill_empty() -> void:
 	while fill_empty_target > get_child_count(): add_display(null)
@@ -36,6 +43,9 @@ func fill_empty() -> void:
 func add_display(stack: ResourceStack) -> void:
 	var node := resource_display.instantiate()
 	node.stack = stack
+	
+	node.grabbed.connect(func (event: InputEvent): grab_item(get_children().find(node)))
+	
 	add_child(node)
 
 func on_appended() -> void:
@@ -50,6 +60,7 @@ func on_removed(index: int) -> void:
 	var node := get_child(index)
 	remove_child(node)
 	node.queue_free()
+	node_removed.emit(index)
 	
 	fill_empty()
 
@@ -63,3 +74,31 @@ func on_fully_modified() -> void:
 	for i in storage.get_content(): add_display(i)
 	
 	fill_empty()
+
+func _gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.is_released():
+		if global.held_item: match (event as InputEventMouseButton).button_index:
+			MOUSE_BUTTON_LEFT:
+				if Input.is_key_pressed(KEY_SHIFT): return transfer_max_held_item()
+				return transfer_held_item()
+			
+			MOUSE_BUTTON_RIGHT: return transfer_half_held_item()
+			MOUSE_BUTTON_MIDDLE: return transfer_one_held_item()
+
+func transfer_held_item() -> void:
+	global.transfer_held_item(storage)
+
+func transfer_half_held_item() -> void:
+	pass
+
+func transfer_one_held_item() -> void:
+	pass
+
+func transfer_max_held_item() -> void:
+	pass
+
+func grab_item(idx: int) -> void:
+	prints("grabbing", idx)
+	global.held_item_storage = storage
+	global.held_item_index = idx
+	prints(global.held_item_storage, global.held_item_index, global.held_item)
