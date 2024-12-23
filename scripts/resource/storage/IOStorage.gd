a83b403f7bf49fba3cdf42df18fa41bc45e12153
TrueSunGaming <79117622+TrueSunGaming@users.noreply.github.com>
2024-12-23 10:50:52 -0500

    feat: implement io storage crafting methods

--- scripts/resource/storage/IOStorage.gd
+++ scripts/resource/storage/IOStorage.gd
@@ -17,3 +17,19 @@ func remove(stack: ResourceStack) -> int:
 func remove_index(idx: int) -> bool:
 	if idx < input.get_content().size(): return input.remove_index(idx)
 	return output.remove_index(idx - input.get_content().size())
+
+func craft(recipe_id: StringName, count := 1) -> bool:
+	if not can_craft(recipe_id, count): return false
+	
+	var recipe := Recipe.get_loaded(recipe_id)
+	for i in recipe.ingredients: input.remove(ResourceStack.new(i.resource_id, i.quantity * count, true))
+	for i in recipe.products: output.add(ResourceStack.new(i.resource_id, i.quantity * count, true))
+	
+	return true
+
+func can_hold_product(recipe_id: StringName, count := 1) -> bool:
+	return output.can_hold_product(recipe_id, count)
+
+func can_craft(recipe_id: StringName, count := 1, ignore_product := false) -> bool:
+	if not ignore_product and not can_hold_product(recipe_id, count): return false
+	return input.can_craft(recipe_id, count, true)
