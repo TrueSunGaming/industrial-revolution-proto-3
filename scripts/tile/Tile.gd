b5b27a6ccf664f6e700f2d4adda72ae2422b547c
TrueSunGaming <79117622+TrueSunGaming@users.noreply.github.com>
2024-12-23 17:06:06 -0500

    feat: implement tile

---
+++ scripts/tile/Tile.gd
@@ -0,0 +1,19 @@
+class_name Tile extends Resource
+
+static var loaded: Dictionary[StringName, Tile]
+
+@export var id: StringName:
+	set(val):
+		if id == val: return
+		if id in loaded: loaded.erase(id)
+		id = val
+		loaded[id] = self
+
+@export var atlas_id: int
+@export var name: String
+
+static func get_loaded(id: StringName) -> Tile:
+	return loaded.get(id)
+
+static func get_atlas_id(id: StringName) -> int:
+	return get_loaded(id).atlas_id
