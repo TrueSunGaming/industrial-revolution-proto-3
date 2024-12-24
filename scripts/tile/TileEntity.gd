41ac9dfdac084c0552f821d875df60e27bf10402
TrueSunGaming <79117622+TrueSunGaming@users.noreply.github.com>
2024-12-24 18:07:43 -0500

    feat: implement tile entity add to map

--- scripts/tile/TileEntity.gd
+++ scripts/tile/TileEntity.gd
@@ -1,9 +1,27 @@
-class_name TileEntity extends Resource
+class_name TileEntity extends RefCounted
 
-@export var origin: Vector2i
+var map: EntityTileMap
+var origin: Vector2i
 
 func tick(delta: float) -> void:
 	pass
 
 func create_data() -> Resource:
 	return null
+
+func add_to_map() -> void:
+	if not map: return push_warning("Could not add TileEntity to map. map is null")
+	
+	map.entities[origin] = self
+	var entity_data := create_data()
+	if entity_data: map.entity_data[origin] = entity_data
+	
+	var size := map.get_tile_size(map.get_cell_source_id(origin))
+	
+	for x in size.x:
+		for y in size.y:
+			var pos := Vector2i(x, y)
+			if pos == Vector2i.ZERO: continue
+			
+			map.entities.erase(pos)
+			map.entity_data.erase(pos)
