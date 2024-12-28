15f467ec8d7f7b1f59bba3a7c7ddacbf6903f941
TrueSunGaming <79117622+TrueSunGaming@users.noreply.github.com>
2024-12-28 18:36:43 -0500

    feat: implement placing tiles
    closes #4

--- scripts/tile/EntityTileMap.gd
+++ scripts/tile/EntityTileMap.gd
@@ -34,6 +34,21 @@ func _unhandled_input(event: InputEvent) -> void:
 
 func handle_click(event: InputEventMouseButton) -> void:
 	var pos := get_tile_origin(local_to_map(get_local_mouse_position()))
-	if not pos in entities: return
+	if not pos in entities: return attempt_place(pos)
 	
 	entities[pos].click(event)
+
+func attempt_place(pos: Vector2i) -> bool:
+	if not global.held_item: return false
+	if global.held_item.quantity <= 0: return false
+	if not global.held_item.resource_data: return false
+	if global.held_item.resource_data.tile not in Tile.loaded: return false
+	
+	var res := place_tile(pos, global.held_item.resource_data.tile, true)
+	
+	global.held_item.quantity -= 1
+	if global.held_item.quantity <= 0:
+		refs.player.inventory.remove_index(global.held_item_index)
+		global.return_held_item()
+	
+	return res
